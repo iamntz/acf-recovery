cad2ba436d1726482ec56fd78f7ee2a65a975068
Ionut Staicu <github@iamntz.com>
2013-12-26 12:19:52 +0200

    initial version

---
+++ readme.md
@@ -0,0 +1,21 @@
+## How To easily recover ACF fields from exported PHP file
+
+#### Prerequisites
+1. Clone this repo on it's own folder, right into theme directory. The name is not important as long as it have it's own folder.
+2. Have your exported fields as `acf_export.php` located one level upper than this script.
+3. Edit `acf_export.php` and replace all instances of `register_field_group` with `re_register_field_group`.
+
+So basically you will have:
+
+```
+ThemeDirectory
+ThemeDirectory\functions.php
+ThemeDirectory\acf_export.php
+ThemeDirectory\acf-export-recover\*
+```
+
+#### Set Up
+1. In your `functions.php` file include the `recover.php`
+2. Access admin with `?recover-acf-fields=1` added in your URL.
+3. Do not reload this page, otherwise you will have duplicates
+4. Enjoy!
---
+++ recover.php
@@ -0,0 +1,52 @@
+<?php
+if( isset( $_GET['recover-acf-fields'] ) ){
+
+  require_once( plugin_dir_path(__FILE__) . '../acf_export.php' );
+  die();
+}
+
+
+function re_register_field_group( $fields ){
+
+  $acf_post = array(
+    "post_type"   => "acf",
+    "post_title"  => $fields['title'] . apply_filters( 'acf/import/title_suffix', " (recover)" ),
+    "menu_order"  => $fields['menu_order'],
+    "post_status" => apply_filters( 'acf/import/post_status', "draft" ),
+  );
+
+  $post_id = wp_insert_post( $acf_post );
+
+  $_POST['fields'] = $fields['fields'];
+  $_POST['location'] = $fields['location'];
+  $_POST['options'] = $fields['options'];
+
+  $i = -1;
+
+  foreach( $fields['fields'] as $key => $field ){
+    $i++;
+    $field['order_no'] = $i;
+    do_action('acf/update_field', $field, $post_id );
+  }
+
+  $fields['location'] = array_values( $fields['location'] );
+
+  foreach( $fields['location'] as $group_id => $group ){
+    if( is_array($group) ) {
+      $group = array_values( $group );
+      foreach( $group as $rule_id => $rule ){
+        $rule['order_no'] = $rule_id;
+        $rule['group_no'] = $group_id;
+        add_post_meta( $post_id, 'rule', $rule );
+      }
+    }
+  }
+
+
+  update_post_meta($post_id, 'position', $fields['options']['position']);
+  update_post_meta($post_id, 'layout', $fields['options']['layout']);
+  update_post_meta($post_id, 'hide_on_screen', $fields['options']['hide_on_screen']);
+
+  echo "<li>Added <strong>{$fields['title']}</strong> with the ID of <strong>{$post_id}</strong></li>\n";
+} // re_register_field_group
+
